1fbfc247951c2d3b3567c73eb10941f783da729b
KimA9210 <allen.s.kim2001@gmail.com>
2019-06-03 01:17:10 -0400

    changes

--- doodlejump.py
+++ doodlejump.py
@@ -4,11 +4,16 @@ import arcade
 import random
 
 # --- Constants ---
-SPRITE_SCALING_PLAYER = 0.4
+SPRITE_SCALING_PLAYER = 0.35
 
 SCREEN_WIDTH = 375
 SCREEN_HEIGHT = 667
 
+"""
+class Platform(arcade.Sprite):
+
+    def update(self):
+"""
 
 class MyGame(arcade.Window):
     """ Custom window class """
@@ -45,15 +50,31 @@ class MyGame(arcade.Window):
         self.player_sprite = arcade.Sprite("images/player.png", SPRITE_SCALING_PLAYER)
         self.player_sprite.center_x = 50
         self.player_sprite.center_y = 150
+        # self.player_sprite.change_x = 0
+        # self.player_sprite.change_y = 0
         self.player_list.append(self.player_sprite)
-
-
 
     def on_draw(self):
         arcade.start_render()
-
         self.player_list.draw()
 
+    """
+    def update(self):
+        self.player_sprite.center_x += self.player_sprite.change_x
+        self.player_sprite.center_y += self.player_sprite.change_y
+    """
+
+    def on_key_press(self, key, modifiers):
+        """ Called whenever a user presses a key """
+        if key == arcade.key.LEFT:
+            self.player_sprite.change_x = -5
+        elif key == arcade.key.RIGHT:
+            self.player_sprite.change_x = 5
+
+    def on_key_release(self, key, modifiers):
+        """ Called whenever a user releases a key. """
+        if key == arcade.key.LEFT or key == arcade.key.RIGHT:
+            self.player_sprite.change_x = 0
 
 
 def main():
