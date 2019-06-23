72102d9c1f404e2802361a9d3d71f88dc5779c6d
KimA9210 <allen.s.kim2001@gmail.com>
2019-06-22 21:04:57 -0400

    done

--- game.py
+++ game.py
@@ -1,29 +1,23 @@
-""" Doodle Jump """
+""" Magikarp Jump """
 
 import arcade
 import random
 
 # --- Constants ---
 SPRITE_SCALING_PLAYER = 0.12
-
+SPRITE_SCALE_PLATFORM = 0.035
 SCREEN_WIDTH = 375
 SCREEN_HEIGHT = 667
-
 PLAYER_MOVEMENT_SPEED = 5
 GRAVITY = 1
-PLAYER_JUMP_SPEED = 15
-
-TITLE_SCREEN = 1
-GAME_START = 2
+PLAYER_JUMP_SPEED = 13
 
 # How many pixels to keep as a minimum margin between the character
-# and the edge of the screen.
-LEFT_VIEWPORT_MARGIN = 150
-RIGHT_VIEWPORT_MARGIN = 150
-BOTTOM_VIEWPORT_MARGIN = 50
+# and the edge of the screen.d
 TOP_VIEWPORT_MARGIN = 100
 
 
+# creating Player class
 class Player(arcade.Sprite):
 
     def __init__(self, filename, sprite_scaling):
@@ -47,12 +41,14 @@ class Player(arcade.Sprite):
             self.kill()
 
 
+# creating Platform class
 class Platform(arcade.Sprite):
 
     def update(self):
         self.center_y -= 1
 
 
+# creating a class for MyGame
 class MyGame(arcade.Window):
     """ Custom window class """
 
@@ -60,25 +56,21 @@ class MyGame(arcade.Window):
         """ Initializer """
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "Doodle Jump")
 
-        # Variables that will hold sprite lists
+        # sprite lists that will hold sprites
         self.player_list = None
         self.platform_list = None
 
         # Setting up player info
         self.player_sprite = None
-        self.score = 0
 
         # physics
         self.physics_engine = None
 
-        # Used to keep track of our scrolling
+        # scrolling
         self.view_bottom = 0
         self.view_left = 0
 
-        # Hide mouse cursor
-        # self.set_mouse_visible(False)
-
-        arcade.set_background_color(arcade.color.WHITE_SMOKE)
+        arcade.set_background_color(arcade.color.BABY_BLUE)
 
     def setup(self):
         """ Set up the game and initialize the variables """
@@ -90,22 +82,14 @@ class MyGame(arcade.Window):
         self.player_list = arcade.SpriteList()
         self.platform_list = arcade.SpriteList()
 
-        # Score
-        self.score = 0
-
         # Setting up the player
         self.player_sprite = Player("images/magikarp.gif", SPRITE_SCALING_PLAYER)
         self.player_sprite.center_x = 50
         self.player_sprite.center_y = 150
-        self.player_sprite.dx = 0
-        self.player_sprite.dy = 0
-        self.player_sprite.GAME_STARTED = False
-        self.player_sprite.GAME_OVER = False
         self.player_list.append(self.player_sprite)
 
         # Platform image from graphicdesign.stackexchange.com
-        platform = Platform("images/platform.png", 0.05)
-
+        platform = Platform("images/ledge.png", SPRITE_SCALE_PLATFORM)
         platform.center_x = 50
         platform.center_y = 110
         self.platform_list.append(platform)
@@ -114,33 +98,45 @@ class MyGame(arcade.Window):
 
         for i in range(100):
             # Platform image from graphicdesign.stackexchange.com
-            platform = Platform("images/platform.png", 0.05)
+            platform = Platform("images/ledge.png", SPRITE_SCALE_PLATFORM)
 
             platform.center_x = random.randrange(SCREEN_WIDTH)
             platform.center_y += y
             self.platform_list.append(platform)
 
-            y += 90
+            y += 130
 
         # Create the 'physics engine'
         self.physics_engine = arcade.PhysicsEngineSimple(self.player_sprite, self.platform_list)
 
     def on_draw(self):
 
+        # render drawings
+        arcade.start_render()
+
         self.physics_engine = arcade.PhysicsEnginePlatformer(self.player_sprite, self.platform_list,
                                                              gravity_constant=GRAVITY)
 
-        arcade.start_render()
+        # Background image from freepik.com
+        bg = arcade.load_texture("images/bg.jpg")
+        scale = 2
+        arcade.draw_texture_rectangle(150, SCREEN_HEIGHT/3 + self.view_bottom, scale * bg.width,
+                                      scale * bg.height, bg, 0)
+
+        # draws all features
         self.platform_list.draw()
         self.player_list.draw()
 
-        # draw score on screen
-        score_text = f"Score: {self.score}"
-        arcade.draw_text(score_text, 10, 10 + self.view_bottom, arcade.color.BLACK, 18)
+        # check if player reaches bottom (y = 0)
+        if self.player_sprite.top < self.view_bottom:
+            self.player_sprite.kill()
+            arcade.draw_text("YOU DIED", SCREEN_WIDTH / 4, SCREEN_HEIGHT / 2 + self.view_bottom, arcade.color.BLACK,
+                             40)
 
     def on_key_press(self, key, modifiers):
         """ Called whenever a user presses a key """
 
+        # move player left & right with A & D
         if key == arcade.key.A:
             self.player_sprite.dx = -PLAYER_MOVEMENT_SPEED
         elif key == arcade.key.D:
@@ -149,40 +145,35 @@ class MyGame(arcade.Window):
     def on_key_release(self, key, modifiers):
         """ Called whenever a user releases a key. """
 
+        # stop player if nothing is pressed / key is released
         if key == arcade.key.A or key == arcade.key.D:
             self.player_sprite.dx = 0
 
     def update(self, delta_time):
 
-        self.player_list.update()
-        self.platform_list.update()
-        self.physics_engine.update()
-
+        # create list of platforms that touch the player
         platform_hit_list = arcade.check_for_collision_with_list(self.player_sprite,
                                                                  self.platform_list)
 
+        # if player touches platform, jump
         for platform in platform_hit_list:
             self.player_sprite.dy = PLAYER_JUMP_SPEED
-            self.score += 1
 
+        # delete platforms that reach the bottom
+        for platform in self.platform_list:
+            if platform.top < self.view_bottom:
+                platform.kill()
 
-        # --- Manage Scrolling ---
+        # update sprite lists
+        self.player_list.update()
+        self.platform_list.update()
 
-        # Track if we need to change the viewport
+        # update physics
+        self.physics_engine.update()
+
+        # --- Scrolling ---
 
         changed = False
-
-        # Scroll left
-        left_boundary = self.view_left + LEFT_VIEWPORT_MARGIN
-        if self.player_sprite.left < left_boundary:
-            self.view_left = 0
-            changed = True
-
-        # Scroll right
-        right_boundary = self.view_left + SCREEN_WIDTH - RIGHT_VIEWPORT_MARGIN
-        if self.player_sprite.right > right_boundary:
-            self.view_left = 0
-            changed = True
 
         # Scroll up
         top_boundary = self.view_bottom + SCREEN_HEIGHT - TOP_VIEWPORT_MARGIN
@@ -190,14 +181,8 @@ class MyGame(arcade.Window):
             self.view_bottom += self.player_sprite.top - top_boundary
             changed = True
 
-        # Scroll down
-        bottom_boundary = self.view_bottom + BOTTOM_VIEWPORT_MARGIN
-        if self.player_sprite.bottom < bottom_boundary:
-            self.view_bottom = 0
-
         if changed:
-            # Only scroll to integers. Otherwise we end up with pixels that
-            # don't line up on the screen
+            # Only scroll to integers
             self.view_bottom = int(self.view_bottom)
             self.view_left = int(self.view_left)
 
